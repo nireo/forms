68da7994f1ca4fd0017c215c0e469622922e12a1
nireo <eemil.lehtikangas1@gmail.com>
2019-12-20 14:50:17 +0200

    added user registeration

---
+++ server/api/v1/auth/auth.controller.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/nireo/forms/server/database/models"
+	"github.com/nireo/forms/server/lib/common"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User alias for model
+type User = models.User
+
+func hash(password string) (string, error) {
+	bytes, err := bcypt.GenerateFromPassword([]byte(password), 12)
+	return string(bytes), err
+}
+
+func checkHash(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func register(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+
+	type RequestBody struct {
+		Username string `json:"username" binding:"required"`
+		Password string `json:"password" binding:"required"`
+	}
+
+	var body RequestBody
+	if err := c.BindJSON(&body); err != nil {
+		c.AbortWithStatus(400)
+		return
+	}
+
+	var exists User
+	if err := db.Where("username = ?", body.Username).First(&exists).Error; err == nil {
+		c.AbortWithStatus(409)
+		return
+	}
+
+	hash, hashErr := hash(body.Password)
+	if hashErr != nil {
+		c.AbortWithStatus(500)
+		return
+	}
+
+	user := User{
+		Username: body.Username,
+		Password: hash,
+	}
+
+	db.NewRecord(user)
+	db.Create(&user)
+
+	serialized := user.Serialize()
+	c.JSON(200, common.JSON{
+		"user":  user.Serialize(),
+		"token": token,
+	})
+}
