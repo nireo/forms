63dc7d8fee0cc50b144eaf516a1f3c0a51d8e8f6
nireo <eemil.lehtikangas1@gmail.com>
2020-01-04 14:32:17 +0200

    added actions and redux thunk

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import questionReducer from './question/reducer';
 import createReducer from './create/reducer';
 import formReducer from './forms/reducer';
 import userReducer from './user/reducer';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   form: questionReducer,
@@ -10,7 +11,7 @@ const rootReducer = combineReducers({
   forms: formReducer,
   user: userReducer
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export type AppState = ReturnType<typeof rootReducer>;
 export default store;
--- client/src/store/user/reducer.ts
+++ client/src/store/user/reducer.ts
@@ -1,4 +1,9 @@
-import { User } from './../../interfaces/User';
+import { Dispatch } from 'redux';
+import { User, UserAction } from './../../interfaces/User';
+import {
+  login as log_in,
+  register as registerUser
+} from '../../services/user.service';
 
 const reducer = (state: null | User = null, action: any) => {
   switch (action.type) {
@@ -8,6 +13,41 @@ const reducer = (state: null | User = null, action: any) => {
       return null;
     default:
       return state;
+  }
+};
+
+export const logout = () => {
+  return { type: 'LOG_OUT' };
+};
+
+export const login = (credentials: UserAction) => {
+  return async (dispatch: Dispatch) => {
+    const user = await log_in(credentials);
+    dispatch({
+      type: 'LOG_IN',
+      data: user
+    });
+  };
+};
+
+export const register = (credentials: UserAction) => {
+  return async (dispatch: Dispatch) => {
+    const user = await registerUser(credentials);
+    dispatch({
+      type: 'LOG_IN',
+      data: user
+    });
+  };
+};
+
+export const checkLocalStorage = () => {
+  const userInfo = localStorage.getItem('user');
+  if (userInfo) {
+    const userInfoJSON = JSON.parse(userInfo);
+    return {
+      type: 'LOG_IN',
+      data: userInfoJSON
+    };
   }
 };
 
