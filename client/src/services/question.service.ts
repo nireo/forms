492511b6c611d0f0b4c45f4d6ee1b6407b3055a9
nireo <eemil.lehtikangas1@gmail.com>
2020-01-14 15:54:27 +0200

    added question service

---
+++ client/src/services/question.service.ts
@@ -0,0 +1,28 @@
+import { Question } from './../interfaces/Question';
+import axios from 'axios';
+
+const baseUrl: string = '/api/question';
+let token: string | null = null;
+
+export const setToken = (newToken: string) => {
+  token = `bearer ${newToken}`;
+};
+
+const getConfig = () => ({
+  headers: { Authorization: token }
+});
+
+export const createQuestion = async (id: string, question: Question) => {
+  const response = await axios.post(`${baseUrl}/${id}`, question, getConfig());
+  return response.data;
+};
+
+export const deleteQuestion = async (id: string) => {
+  const response = await axios.delete(`${baseUrl}/${id}`, getConfig());
+  return response.data;
+};
+
+export const updateQuestion = async (id: string, question: Question) => {
+  const response = await axios.patch(`${baseUrl}/${id}`, question, getConfig());
+  return response.data;
+};
