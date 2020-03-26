bf1fdb627c6e96e89c22ef949dc1c8f93d88a036
nireo <eemil.lehtikangas1@gmail.com>
2020-03-26 15:55:16 +0200

    added a better single question interface

--- client/src/components/Answers/AnswersMain.tsx
+++ client/src/components/Answers/AnswersMain.tsx
@@ -1,29 +1,32 @@
-import React, { useEffect, useState, useCallback } from "react";
-import Paper from "@material-ui/core/Paper";
-import Container from "@material-ui/core/Container";
-import { makeStyles, Theme } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import React, { useEffect, useState, useCallback } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import {
   getAnswer,
   removeAnswer as serviceRemoveAnswer
-} from "../../services/answer.service";
-import { Loading } from "../Layout/Loading";
-import Card from "@material-ui/core/Card";
-import Grid from "@material-ui/core/Grid";
-import CardContent from "@material-ui/core/CardContent";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import { Link } from "react-router-dom";
+} from '../../services/answer.service';
+import { Loading } from '../Layout/Loading';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { Link } from 'react-router-dom';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackwardIosIcon from '@material-ui/icons/ArrowBackIos';
+import { ViewAnswer } from './ViewAnswer';
 
 const useStyles = makeStyles((theme: Theme) => ({
   layout: {
-    width: "auto",
+    width: 'auto',
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
       width: 600,
-      marginLeft: "auto",
-      marginRight: "auto"
+      marginLeft: 'auto',
+      marginRight: 'auto'
     }
   },
   paper: {
@@ -49,6 +52,7 @@ export const AnswersMain: React.FC<Props> = props => {
   const classes = useStyles(props);
   const [loaded, setLoaded] = useState<boolean>(false);
   const [answers, setAnswers] = useState<any>([]);
+  const [selected, setSelected] = useState<number>(1);
 
   const getAnswers = useCallback(async () => {
     const data: any = await getAnswer(props.id);
@@ -65,84 +69,68 @@ export const AnswersMain: React.FC<Props> = props => {
   const returnSensibleDate = (dateString: string) => {
     const date = new Date(dateString);
     var monthNames = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
     ];
 
     var day = date.getDate();
     var monthIndex = date.getMonth();
     var year = date.getFullYear();
 
-    return day + " " + monthNames[monthIndex] + " " + year;
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
   };
 
   const removeAnswer = async (id: string) => {
-    if (window.confirm("Are you sure you want to delete the answer?")) {
+    if (window.confirm('Are you sure you want to delete the answer?')) {
       await serviceRemoveAnswer(id);
       setAnswers(answers.filter((item: any) => item.uuid !== id));
     }
   };
 
+  console.log(answers);
+
   return (
     <Container maxWidth="md">
       <Paper className={classes.paper}>
         <Typography variant="h4">Answers</Typography>
+        <div style={{ display: 'flex' }}>
+          {selected !== 1 && (
+            <IconButton onClick={() => setSelected(selected - 1)}>
+              <ArrowBackwardIosIcon />
+            </IconButton>
+          )}
+          <Typography
+            style={{
+              marginTop: '0.8rem',
+              marginLeft: '0.8rem',
+              marginRight: '0.8rem'
+            }}
+          >
+            {selected}/{answers.length}
+          </Typography>
+          {selected !== answers.length && (
+            <IconButton onClick={() => setSelected(selected + 1)}>
+              <ArrowForwardIosIcon />
+            </IconButton>
+          )}
+        </div>
         {loaded === false ? (
           <Loading />
         ) : (
           <div>
-            {answers.map((answer: any) => (
-              <div key={answer.uuid} style={{ marginTop: "0.5rem" }}>
-                <Card>
-                  <CardContent>
-                    <Grid container>
-                      <Grid item xs={11}>
-                        <div>
-                          <Typography variant="h6">{answer.uuid}</Typography>
-                        </div>
-                        <div>
-                          <Typography>
-                            {returnSensibleDate(answer.created_at)}
-                          </Typography>
-                        </div>
-                        <div style={{ marginTop: "0.5rem" }}>
-                          <Link
-                            to={`/answer/${answer.uuid}`}
-                            style={{
-                              textDecoration: "none",
-                              fontSize: "14px",
-                              color: "#ff9999"
-                            }}
-                          >
-                            View
-                          </Link>
-                        </div>
-                      </Grid>
-                      <Grid item xs={1}>
-                        <IconButton
-                          style={{ color: "#ff9999" }}
-                          component="span"
-                          aria-label="delete-answer"
-                          onClick={() => removeAnswer(answer.uuid)}
-                        >
-                          <DeleteIcon />
-                        </IconButton>
-                      </Grid>
-                    </Grid>
-                  </CardContent>
-                </Card>
-              </div>
-            ))}
+            {answers.length > 0 && (
+              <ViewAnswer id={answers[selected - 1].uuid} />
+            )}
           </div>
         )}
       </Paper>
