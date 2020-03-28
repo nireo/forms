15194bbd8b9f0879eebbda7a48fc74d3921f5865
nireo <eemil.lehtikangas1@gmail.com>
2020-03-28 12:05:20 +0200

    fixed a bug with react-scripts

--- client/src/components/Answers/AllAnswers.tsx
+++ client/src/components/Answers/AllAnswers.tsx
@@ -4,12 +4,17 @@ import { Pie } from 'react-chartjs-2';
 export const AllAnswers: React.FC = () => {
   const [data] = useState<any>({
     labels: ['1', '2', '3', '4'],
-    data: [25, 25, 25, 25],
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.6)',
-      'rgba(54, 162, 235, 0.6)',
-      'rgba(255, 206, 86, 0.6)',
-      'rgba(75, 192, 192, 0.6)'
+    datasets: [
+      {
+        label: '% of answers',
+        data: [25, 25, 25, 25],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)'
+        ]
+      }
     ]
   });
 
