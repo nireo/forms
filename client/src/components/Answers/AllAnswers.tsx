917b1ed7d5a280bb11db9f4f1b4f80ece89abe4c
nireo <eemil.lehtikangas1@gmail.com>
2020-03-28 12:55:50 +0200

    added a pie chart component

--- client/src/components/Answers/AllAnswers.tsx
+++ client/src/components/Answers/AllAnswers.tsx
@@ -1,26 +1,14 @@
 import React, { useState } from 'react';
-import { Pie } from 'react-chartjs-2';
+import { PieChart } from './PieChart';
 
 export const AllAnswers: React.FC = () => {
-  const [data] = useState<any>({
-    labels: ['1', '2', '3', '4'],
-    datasets: [
-      {
-        label: '% of answers',
-        data: [25, 25, 25, 25],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.6)',
-          'rgba(54, 162, 235, 0.6)',
-          'rgba(255, 206, 86, 0.6)',
-          'rgba(75, 192, 192, 0.6)'
-        ]
-      }
-    ]
-  });
-
   return (
     <div>
-      <Pie data={data} />
+      <PieChart
+        numberData={[25, 25, 25, 25]}
+        labels={['1', '2', '3', '4']}
+        label="% of answers"
+      />
     </div>
   );
 };
---
+++ client/src/components/Answers/PieChart.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { Pie } from 'react-chartjs-2';
+
+type Props = {
+  numberData: number[];
+  labels: string[];
+  label: string;
+};
+
+export const PieChart: React.FC<Props> = ({ numberData, labels, label }) => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    if (!data) {
+      setData({
+        labels,
+        datasets: [
+          {
+            label,
+            data: numberData,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)'
+            ]
+          }
+        ]
+      });
+    }
+  });
+
+  return (
+    <div>
+      <Pie data={data} />
+    </div>
+  );
+};
