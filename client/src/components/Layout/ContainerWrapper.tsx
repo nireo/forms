d69e12787c648d30193124c8f8deebd67262e650
nireo <eemil.lehtikangas1@gmail.com>
2020-04-04 20:27:14 +0300

    added a containerwrapper

---
+++ client/src/components/Layout/ContainerWrapper.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    layout: {
+      width: 'auto',
+      marginLeft: theme.spacing(2),
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+        width: 600,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      },
+    },
+    paper: {
+      marginTop: theme.spacing(3),
+      marginBottom: theme.spacing(3),
+      padding: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+        marginTop: theme.spacing(6),
+        marginBottom: theme.spacing(6),
+        padding: theme.spacing(3),
+      },
+    },
+  })
+);
+
+export const ContainerWrapper: React.FC = ({ children }) => {
+  const classes = useStyles();
+  return (
+    <Container maxWidth="md">
+      <Paper className={classes.paper}>{children}</Paper>
+    </Container>
+  );
+};
