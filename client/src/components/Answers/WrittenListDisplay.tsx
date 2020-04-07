3c3b649f590f24491c84cfc90533725c51a72d5e
nireo <eemil.lehtikangas1@gmail.com>
2020-04-07 19:54:21 +0300

    added proper checkboxes

--- client/src/components/Answers/WrittenListDisplay.tsx
+++ client/src/components/Answers/WrittenListDisplay.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,7 +21,21 @@ type Props = {
 };
 
 export const WrittenListDisplay: React.FC<Props> = ({ answers }) => {
+  const [checked, setChecked] = useState<string[]>([]);
   const classes = useStyles();
+
+  const handleChange = (answer: string) => () => {
+    const currentIndex = checked.indexOf(answer);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(answer);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
 
   return (
     <List dense className={classes.root}>
@@ -30,12 +45,19 @@ export const WrittenListDisplay: React.FC<Props> = ({ answers }) => {
             <ListItem key={answer} button>
               <ListItemText id={answer} primary={answer} />
               <ListItemSecondaryAction>
-                <Checkbox edge="end" checked={true} />
+                <Checkbox
+                  edge="end"
+                  checked={checked.indexOf(answer) !== -1}
+                  onChange={handleChange(answer)}
+                />
               </ListItemSecondaryAction>
             </ListItem>
           )}
         </div>
       ))}
+      {checked.length > 0 && (
+        <button variant="contained">Delete selected</button>
+      )}
     </List>
   );
 };
