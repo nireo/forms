3a39394e288e2bca00be7ee21228e14791ced33f
nireo <eemil.lehtikangas1@gmail.com>
2020-04-09 21:48:27 +0300

    added multiple choice parsing

--- client/src/utils/FormatAllAsnwerData.ts
+++ client/src/utils/FormatAllAsnwerData.ts
@@ -1,14 +1,41 @@
-import {
-  QuestionWithAnswers,
-  Data as DataInterface,
-  Answer,
-} from '../components/Answers/AllAnswers';
-import { Question } from '../interfaces/Question';
+import { NewAnswer as Answer } from '../interfaces/Answer';
+import { Question, QuestionWithAnswers } from '../interfaces/Question';
+import { Data as DataInterface } from '../interfaces/Data';
 
 const formatData = (data: DataInterface): QuestionWithAnswers[] => {
   let questionsWithAnswers: QuestionWithAnswers[] = [];
   data?.questions.forEach((question: Question) => {
-    if (question.answerType === 2) {
+    if (question.answerType === 1) {
+      const newQuestionWithAnswers: QuestionWithAnswers = {
+        questionID: question.temp_uuid,
+        type: question.answerType,
+        question: question.question,
+        amounts: [],
+        labels: [],
+      };
+
+      data?.answers.forEach((answer: Answer) => {
+        let index = newQuestionWithAnswers.labels?.indexOf(answer.answers);
+        if (
+          index !== -1 &&
+          index !== undefined &&
+          newQuestionWithAnswers.amounts !== undefined
+        ) {
+          newQuestionWithAnswers.amounts[index] += 1;
+        } else if (index !== undefined) {
+          newQuestionWithAnswers.labels = newQuestionWithAnswers.labels?.concat(
+            answer.answers
+          );
+          newQuestionWithAnswers.amounts = newQuestionWithAnswers.amounts?.concat(
+            1
+          );
+        }
+      });
+
+      questionsWithAnswers = questionsWithAnswers.concat(
+        newQuestionWithAnswers
+      );
+    } else if (question.answerType === 2) {
       let newQuestionWithAnswers: QuestionWithAnswers = {
         questionID: question.temp_uuid,
         type: question.answerType,
