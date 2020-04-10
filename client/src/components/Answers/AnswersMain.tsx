44848f57a0da33f1cf2955b32b6467e772de7944
nireo <eemil.lehtikangas1@gmail.com>
2020-04-10 12:49:08 +0300

    added tabs for single and all answers

--- client/src/components/Answers/AnswersMain.tsx
+++ client/src/components/Answers/AnswersMain.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import {
   getAnswer,
@@ -12,6 +11,8 @@ import ArrowBackwardIosIcon from '@material-ui/icons/ArrowBackIos';
 import { ViewAnswer } from './ViewAnswer';
 import { AllAnswers } from './AllAnswers';
 import { ContainerWrapper } from '../Layout/ContainerWrapper';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 
 type Props = {
   id: string;
@@ -21,6 +22,7 @@ export const AnswersMain: React.FC<Props> = (props) => {
   const [loaded, setLoaded] = useState<boolean>(false);
   const [answers, setAnswers] = useState<any>([]);
   const [selected, setSelected] = useState<number>(1);
+  const [page, setPage] = useState<string>('all');
 
   const getAnswers = useCallback(async () => {
     const data: any = await getAnswer(props.id);
@@ -44,44 +46,61 @@ export const AnswersMain: React.FC<Props> = (props) => {
   return (
     <ContainerWrapper>
       <Typography variant="h4">Answers</Typography>
-      <div style={{ display: 'flex' }}>
-        {selected !== 1 && (
-          <IconButton onClick={() => setSelected(selected - 1)}>
-            <ArrowBackwardIosIcon />
-          </IconButton>
-        )}
-        <Typography
-          style={{
-            marginTop: '0.8rem',
-            marginLeft: '0.8rem',
-            marginRight: '0.8rem',
-          }}
-        >
-          {selected}/{answers.length}
-        </Typography>
-        {selected !== answers.length && (
-          <IconButton onClick={() => setSelected(selected + 1)}>
-            <ArrowForwardIosIcon />
-          </IconButton>
-        )}
-      </div>
+      <Divider />
+      {page === 'single' ? (
+        <Button variant="contained" onClick={() => setPage('all')}>
+          Show all answers
+        </Button>
+      ) : (
+        <Button variant="contained" onClick={() => setPage('single')}>
+          Show single answers
+        </Button>
+      )}
+      {page === 'single' && (
+        <div style={{ display: 'flex' }}>
+          {selected !== 1 && (
+            <IconButton onClick={() => setSelected(selected - 1)}>
+              <ArrowBackwardIosIcon />
+            </IconButton>
+          )}
+          <Typography
+            style={{
+              marginTop: '0.8rem',
+              marginLeft: '0.8rem',
+              marginRight: '0.8rem',
+            }}
+          >
+            {selected}/{answers.length}
+          </Typography>
+          {selected !== answers.length && (
+            <IconButton onClick={() => setSelected(selected + 1)}>
+              <ArrowForwardIosIcon />
+            </IconButton>
+          )}
+        </div>
+      )}
       {loaded === false ? (
         <Loading />
       ) : (
         <div>
-          {answers.length > 0 && (
+          {page === 'single' ? (
             <div>
-              <div>
-                <ViewAnswer
-                  answer={answers[selected - 1]}
-                  id={answers[selected - 1].uuid}
-                />
-              </div>
+              {answers.length > 0 && (
+                <div>
+                  <div>
+                    <ViewAnswer
+                      answer={answers[selected - 1]}
+                      id={answers[selected - 1].uuid}
+                    />
+                  </div>
+                </div>
+              )}
             </div>
+          ) : (
+            <AllAnswers id={props.id} />
           )}
         </div>
       )}
-      <AllAnswers id={props.id} />
     </ContainerWrapper>
   );
 };
