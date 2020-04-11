58774632b2cd63cbdc3afb16e28e7b709cb0b520
nireo <eemil.lehtikangas1@gmail.com>
2020-04-11 20:39:59 +0300

    added the select button

--- client/src/components/Answers/SingleQuestion.tsx
+++ client/src/components/Answers/SingleQuestion.tsx
@@ -1,10 +1,14 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, ChangeEvent } from 'react';
 import Select from '@material-ui/core/Select';
 import { NewAnswer as Answer } from '../../interfaces/Answer';
 import { Data as DataInterface } from '../../interfaces/Data';
 import { Question } from '../../interfaces/Question';
 import { allAnswers } from '../../services/answer.service';
 import { ContainerWrapper } from '../Layout/ContainerWrapper';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
 
 type Props = {
   id: string;
@@ -13,7 +17,7 @@ type Props = {
 export const SingleQuestion: React.FC<Props> = ({ id }) => {
   const [loaded, setLoaded] = useState<boolean>(false);
   const [data, setData] = useState<DataInterface | null>(null);
-  const [selected, setSelected] = useState<Question | null>(null);
+  const [selected, setSelected] = useState<string>('');
 
   const loadData = useCallback(async () => {
     const loadedData = await allAnswers(id);
@@ -26,7 +30,41 @@ export const SingleQuestion: React.FC<Props> = ({ id }) => {
         setLoaded(true);
       });
     }
+
+    if (data !== null && selected === '' && data.questions.length > 0) {
+      setSelected(data.questions[0].temp_uuid);
+    }
   }, [data, loaded]);
 
-  return <ContainerWrapper></ContainerWrapper>;
+  const handleQuestionChange = (event: ChangeEvent<{ value: unknown }>) => {
+    setSelected(event.target.value as string);
+  };
+
+  return (
+    <div>
+      <ContainerWrapper>
+        {data === null && <CircularProgress />}
+        {data !== null && (
+          <div>
+            <Typography variant="h6">Select question</Typography>
+            <FormControl variant="outlined">
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                value={selected}
+                onChange={handleQuestionChange}
+                style={{ width: '400px' }}
+              >
+                {data.questions.map((question: Question) => (
+                  <MenuItem value={question.temp_uuid}>
+                    {question.question}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+        )}
+      </ContainerWrapper>
+      {selected !== '' && <ContainerWrapper>hello</ContainerWrapper>}
+    </div>
+  );
 };
