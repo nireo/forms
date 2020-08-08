75da7cc616452277138bb36d31d55ef1a45d4235
nireo <eemil.lehtikangas1@gmail.com>
2020-08-08 15:26:15 +0300

    started adding test

--- server/lib/common/setupRouter.go
+++ server/lib/common/setupRouter.go
@@ -1,31 +1,13 @@
-package main
+package common
 
 import (
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
-	"github.com/nireo/forms/server/lib/common"
+	"github.com/joho/godotenv"
 	"github.com/nireo/forms/server/database"
 	"github.com/nireo/forms/server/lib/middlewares"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
-func main() {
-	// load environment variables
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	app := SetupRouter()
-	app.Run(":" + port)
-}
 func SetupRouter() *gin.Engine {
 	err := godotenv.Load()
 	if err != nil {
